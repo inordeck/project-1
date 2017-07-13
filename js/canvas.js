b0566279859ef9fa65303383c602cdddfefd8c4d
inordeck <ian.nordeck@gmail.com>
2017-07-13 15:11:16 -0600

    add reset button, player badge and placeholders for best time and timer

--- js/canvas.js
+++ js/canvas.js
@@ -10,67 +10,73 @@ link.type = 'text/css';
 link.href = 'https://fonts.googleapis.com/css?family=Bungee+Inline|Bungee+Outline|Raleway:300,800';
 document.getElementsByTagName('head')[0].appendChild(link);
 
-/* MAZE */
-/* maze start */
-var font, x, y;
-font = 100;
-x = 130;
-y = 90;
-this.ctx.font = font + 'px "Bungee Outline"';
-this.ctx.fillStyle = 'gray';
-this.ctx.save();
-this.ctx.translate(x, y);
-this.ctx.rotate(Math.PI / 2);
-this.ctx.textAlign = 'center';
-this.ctx.fillText('start', x, y);
-this.ctx.restore();
+function drawMaze() {
+	/* MAZE */
+	/* maze start */
+	var font, x, y;
+	font = 100;
+	x = 130;
+	y = 90;
+	this.ctx.font = font + 'px "Bungee Outline"';
+	this.ctx.fillStyle = 'gray';
+	this.ctx.save();
+	this.ctx.translate(x, y);
+	this.ctx.rotate(Math.PI / 2);
+	this.ctx.textAlign = 'center';
+	this.ctx.fillText('start', x, y);
+	this.ctx.restore();
 
-/* maze finish */
-var font, x, y;
-font = 90;
-x = 590;
-y = -380;
-this.ctx.font = font + 'px "Bungee Outline"';
-this.ctx.fillStyle = 'gray';
-this.ctx.save();
-this.ctx.translate(x, y);
-this.ctx.rotate(Math.PI / 2);
-this.ctx.textAlign = 'center';
-this.ctx.fillText('finish', x, y);
-this.ctx.restore();
+	/* maze finish */
+	var font, x, y;
+	font = 90;
+	x = 590;
+	y = -380;
+	this.ctx.font = font + 'px "Bungee Outline"';
+	this.ctx.fillStyle = 'gray';
+	this.ctx.save();
+	this.ctx.translate(x, y);
+	this.ctx.rotate(Math.PI / 2);
+	this.ctx.textAlign = 'center';
+	this.ctx.fillText('finish', x, y);
+	this.ctx.restore();
 
-/* maze frame */
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, 1080, 20); // top wall
-ctx.fillRect(0, 400, 1080, 20); // bottom wall
-ctx.fillRect(0, 0, 20, 420); // left wall
-ctx.fillRect(1060, 0, 20, 420); // right wall
-ctx.fillRect(130, 0, 20, 260); // inner left top
-ctx.fillRect(130, 320, 20, 100); // inner left bottom
-ctx.fillRect(930, 0, 20, 260); // inner right top
-ctx.fillRect(930, 320, 20, 100); // inner right bottom
+	/* maze frame */
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, 1080, 20); // top wall
+	ctx.fillRect(0, 400, 1080, 20); // bottom wall
+	ctx.fillRect(0, 0, 20, 420); // left wall
+	ctx.fillRect(1060, 0, 20, 420); // right wall
+	ctx.fillRect(130, 0, 20, 260); // inner left top
+	ctx.fillRect(130, 320, 20, 100); // inner left bottom
+	ctx.fillRect(930, 0, 20, 260); // inner right top
+	ctx.fillRect(930, 320, 20, 100); // inner right bottom
 
-/* vertical maze walls */
-ctx.fillRect(210, 80, 20, 100);
-ctx.fillRect(290, 0, 20, 100);
-ctx.fillRect(290, 160, 20, 100);
-ctx.fillRect(370, 80, 20, 100);
-ctx.fillRect(370, 320, 20, 100);
-ctx.fillRect(450, 160, 20, 180);
-ctx.fillRect(530, 80, 20, 180);
-ctx.fillRect(610, 80, 20, 100);
-ctx.fillRect(850, 80, 20, 180);
+	/* vertical maze walls */
+	ctx.fillRect(210, 80, 20, 100);
+	ctx.fillRect(290, 0, 20, 100);
+	ctx.fillRect(290, 160, 20, 100);
+	ctx.fillRect(370, 80, 20, 100);
+	ctx.fillRect(370, 320, 20, 100);
+	ctx.fillRect(450, 160, 20, 180);
+	ctx.fillRect(530, 80, 20, 180);
+	ctx.fillRect(610, 80, 20, 100);
+	ctx.fillRect(850, 80, 20, 180);
 
-/* horizontal maze walls */
-ctx.fillRect(210, 80, 100, 20);
-ctx.fillRect(370, 80, 180, 20);
-ctx.fillRect(610, 80, 260, 20);
-ctx.fillRect(610, 160, 180, 20);
-ctx.fillRect(130, 240, 180, 20);
-ctx.fillRect(370, 240, 100, 20);
-ctx.fillRect(530, 240, 420, 20);
-ctx.fillRect(130, 320, 180, 20);
-ctx.fillRect(450, 320, 500, 20);
+	/* horizontal maze walls */
+	ctx.fillRect(210, 80, 100, 20);
+	ctx.fillRect(370, 80, 180, 20);
+	ctx.fillRect(610, 80, 260, 20);
+	ctx.fillRect(610, 160, 180, 20);
+	ctx.fillRect(130, 240, 180, 20);
+	ctx.fillRect(370, 240, 100, 20);
+	ctx.fillRect(530, 240, 420, 20);
+	ctx.fillRect(130, 320, 180, 20);
+	ctx.fillRect(450, 320, 500, 20);
+
+} // end of draw maze function
+
+drawMaze();
+
 
 
 /* PLAYER 1 */
@@ -78,11 +84,12 @@ var p1x = 75;
 var p1y = 50;
 var p1Radius = 20;
 
+
 drawP1();
 
 function drawP1() {
 	// clear the canvas for the next frame
-//	ctx.clearRect(0,0,canvas.width,canvas.height); // ERROR: clearing game board
+	//ctx.clearRect(0,0,canvas.width,canvas.height); // ERROR: clearing game board
 	/* body */
 	ctx.beginPath();
 	ctx.arc(p1x, p1y, p1Radius, 0, Math.PI * 2, false);
