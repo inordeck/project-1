a1381f2ad1a4ba0f63b3b23366ee6da14f4a7b0e
inordeck <ian.nordeck@gmail.com>
2017-07-14 13:46:33 -0600

    fix error of clearing whole gameboard on player movement

--- js/canvas.js
+++ js/canvas.js
@@ -1,5 +1,6 @@
 console.log("is there anybody, out there...");
 
+/*  set up canvas */
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
@@ -10,9 +11,10 @@ link.type = 'text/css';
 link.href = 'https://fonts.googleapis.com/css?family=Bungee+Inline|Bungee+Outline|Raleway:300,800';
 document.getElementsByTagName('head')[0].appendChild(link);
 
+
+/* MAZE */
 function drawMaze() {
-	/* MAZE */
-	/* maze start */
+/* maze start */
 	var font, x, y;
 	font = 100;
 	x = 130;
@@ -25,8 +27,7 @@ function drawMaze() {
 	this.ctx.textAlign = 'center';
 	this.ctx.fillText('start', x, y);
 	this.ctx.restore();
-
-	/* maze finish */
+/* maze finish */
 	var font, x, y;
 	font = 90;
 	x = 590;
@@ -39,8 +40,7 @@ function drawMaze() {
 	this.ctx.textAlign = 'center';
 	this.ctx.fillText('finish', x, y);
 	this.ctx.restore();
-
-	/* maze frame */
+/* maze frame */
 	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, 1080, 20); // top wall
 	ctx.fillRect(0, 400, 1080, 20); // bottom wall
@@ -50,8 +50,7 @@ function drawMaze() {
 	ctx.fillRect(130, 320, 20, 100); // inner left bottom
 	ctx.fillRect(930, 0, 20, 260); // inner right top
 	ctx.fillRect(930, 320, 20, 100); // inner right bottom
-
-	/* vertical maze walls */
+/* vertical maze walls */
 	ctx.fillRect(210, 80, 20, 100);
 	ctx.fillRect(290, 0, 20, 100);
 	ctx.fillRect(290, 160, 20, 100);
@@ -61,8 +60,7 @@ function drawMaze() {
 	ctx.fillRect(530, 80, 20, 180);
 	ctx.fillRect(610, 80, 20, 100);
 	ctx.fillRect(850, 80, 20, 180);
-
-	/* horizontal maze walls */
+/* horizontal maze walls */
 	ctx.fillRect(210, 80, 100, 20);
 	ctx.fillRect(370, 80, 180, 20);
 	ctx.fillRect(610, 80, 260, 20);
@@ -72,11 +70,8 @@ function drawMaze() {
 	ctx.fillRect(530, 240, 420, 20);
 	ctx.fillRect(130, 320, 180, 20);
 	ctx.fillRect(450, 320, 500, 20);
-
 } // end of draw maze function
-
 drawMaze();
-
 
 
 /* PLAYER 1 */
@@ -84,12 +79,8 @@ var p1x = 75;
 var p1y = 50;
 var p1Radius = 20;
 
-
-drawP1();
-
-function drawP1() {
+function player1() {
 	// clear the canvas for the next frame
-	//ctx.clearRect(0,0,canvas.width,canvas.height); // ERROR: clearing game board
 	/* body */
 	ctx.beginPath();
 	ctx.arc(p1x, p1y, p1Radius, 0, Math.PI * 2, false);
@@ -97,44 +88,65 @@ function drawP1() {
 	ctx.fillStyle = 'orange';
 	ctx.fill();
 }
+player1();
+
+/* write function to clear canvas */
+function clear(){
+	ctx.clearRect(0,0,canvas.width,canvas.height); // ERROR: clearing game board
+}
 
 window.addEventListener("keyDown", keyDownHandler, true); 
-    // Here we just handle command keys
 function keyDownHandler(event) {
-	// get which key the user pressed
+	clear();
 	var key=event.which;
-	// Let keypress handle displayable characters
 	if(key>46){ return; }
 	switch(key){
 		case 37:  // left key
-		// move the ball left by subtracting from p1X
+		// move the ball left 
         p1x -= 20;
         break;
-
         case 39:  // right key
-        // move the ball right by adding to p1X
+        // move the ball right 
         p1x += 20;
         break;
-
         case 38:  // up key
-		// move the ball left by subtracting from p1X
+		// move the ball left 
         p1y -= 20;
         break;
-
         case 40:  // down key
-		// move the ball left by subtracting from p1X
+		// move the ball left 
         p1y += 20;
         break;
-
-    default:
-        break;
     }
-	drawP1();
-}  // end of event handler function
+	player1();
+	drawMaze();
+}  // end of key handler function
 
 window.addEventListener("keydown", keyDownHandler, true);
 
+function collisionDetection() {
+	/* outside edges */
+	/* outside edge top */
+	if (p1Radis.y > 40) {
+		p1Radis.y = 40;
+	}
+}
+collisionDetection();
+/*var startCountdown = setInterval(function(){
+$("#count_num").html(function(i,html){
+   if(parseInt(html)>1)
+   {
+   return parseInt(html)-1;
+   }
+   else
+   {
+   clearTimeout(timer);
+   return "GO!";
+   }
+ });
+//startCountdown();
 
+},1000);
 
 /* PLAYER 2 
 function drawP2(){ */
